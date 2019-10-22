abbd3d86b90910e92ca855b0b6654df91b339010
lbaleman <luisba93@gmail.com>
2019-10-22 20:46:00 +0100

    Programa version 2

--- CarreraTortugas.py
+++ CarreraTortugas.py
@@ -1,4 +1,5 @@
 import turtle
+import random
 
 class Circuito():
     corredores=[]
@@ -11,7 +12,7 @@ class Circuito():
         self.__screen.setup(width, height)
         self.__screen.bgcolor('lightgray')
         self.__startLine = -width/2 +20
-        self.__finishLine = width/2 -20
+        self.__finishLine = width/2 -100
         
         self.__createrunners()
     
@@ -24,9 +25,27 @@ class Circuito():
             newTurtle.setpos(self.__startLine, i*20-30)
             self.corredores.append(newTurtle)
             
+    def getganador(self,valor):
+        return self.corredores[valor].color()
+            
+    def competir(self):
+        hayGanador = False
         
+        while not hayGanador:
+            for i in self.corredores:
+                avance=random.randint(1,6)
+                i.forward(avance)
+                
+                
+                if i.position()[0] >= self.__finishLine:
+                    hayGanador= True
+                    print(i.color()[0])
+                
+
+            
+    
 if __name__ == '__main__':
     circuito = Circuito(640, 480)
-
+    circuito.competir()
         
     
